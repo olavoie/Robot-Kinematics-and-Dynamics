c3d8740b86660774283b3273631ac15def4aacec
olavoie <olavoie9507@gmail.com>
2018-10-21 16:45:23 -0400

    adding comptue jacobian

--- src/ForwardKinematic.m
+++ src/ForwardKinematic.m
@@ -1,14 +1,40 @@
 classdef ForwardKinematic
     properties
         mRobotParam = RobotParam 
+        mFrame;
+        mJacobian = zeros(6,7);
     end
     methods
-        function T07 = updateJointFrame(obj, jointAngles)
-            obj.updateJoint(jointAngles, 1)
-            T07 = obj.updateJoint(jointAngles, 1) * obj.updateJoint(jointAngles, 2) * obj.updateJoint(jointAngles, 3) * obj.updateJoint(jointAngles, 4) * obj.updateJoint(jointAngles, 5) * obj.updateJoint(jointAngles, 6) * obj.updateJoint(jointAngles, 7);
+        function obj = updateJointFrame(obj, jointAngles)
+            obj.mFrame(:, :, 1) = obj.updateJoint(jointAngles, 1);
+            
+            for n = 2:obj.mRobotParam.mActuatorCount
+                obj.mFrame(:, :, n) = obj.mFrame(:, :, n - 1) * obj.updateJoint(jointAngles, n);
+            end
+            
+            obj.mJacobian = obj.updateJacobian(obj.mFrame);
         end
+        
         function T = updateJoint(obj, angle, joint)
             T = obj.mRobotParam.mJointReferentiel(:, :, joint) * rotm2tform(rotz(angle(joint) + obj.mRobotParam.mJointOffset(joint)));
+        end
+        
+        function jacobian = updateJacobian(obj, frame)
+            
+            for k = 1:obj.mRobotParam.mActuatorCount
+                rotMatrix = tform2rotm(frame(:, :, k));
+                angulaVelocity = rotMatrix(:, 3).';
+                linearVelocity = obj.computeLinearVelocity(rotMatrix, frame, k);                
+                obj.mJacobian(:, k) = [linearVelocity, angulaVelocity];
+            end
+            
+            jacobian = obj.mJacobian;
+        end
+        
+        function v = computeLinearVelocity(obj, rotMatrix, frame, joint)
+            Pi = tform2trvec(frame(:, :, joint)) - tform2trvec(frame(:, :, obj.mRobotParam.mActuatorCount));
+            ai = rotMatrix(:, 3);
+            v = cross(Pi, ai);
         end
     end
 end
